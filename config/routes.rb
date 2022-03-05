2d2ec4a0fcb4a8c0dee6d2db688d3b69826bb63b
rordev <rordev@gmail.com>
2022-03-05 15:40:29 +0530

    pawan prj

---
+++ config/routes.rb
@@ -0,0 +1,31 @@
+Rails.application.routes.draw do
+      resources :users, only: [:new, :create]
+   get 'login', to: 'sessions#new'
+   post 'login', to: 'sessions#create'
+   get 'welcome', to: 'sessions#welcome'
+   get 'authorized', to: 'sessions#page_requires_login'
+   
+   
+      delete '/logout' => 'sessions#destroy'
+  
+    
+
+
+  root "products#index"
+     resources :products do
+     resources :comments
+  end
+
+  resources :shops
+
+  resources :carts do
+      post 'add_to_cart/:product_id', to: 'carts#add_to_cart', as: 'add_to_cart'
+  end
+
+  resources :likes, only:[:create,:destroy]
+
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
